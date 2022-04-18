0dd227d36ee0b3551a9bcc03a40601b379e42b97
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 18:25:23 -0500

    just get all from attendees

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -1,8 +1,9 @@
 <?php
 include './include.php';
 
-$sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
+// $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
+$sql = "SELECT * FROM `attendees`";
 
-sql_submit( $sql );
+json_return( sql_submit( $sql ) );
 
 ?>
