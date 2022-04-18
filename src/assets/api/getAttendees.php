675316335d45c77ebb942e7bf7512fee3aa6db7c
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 18:11:07 -0500

    x2

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -1,6 +1,5 @@
 <?php
-include './db/db_scr.php';
-include './f(x)/script.php';
+include './include.php';
 
 $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 
