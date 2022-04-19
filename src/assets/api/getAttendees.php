05d9b9d927204b9bf7bf6130821519d0035d5638
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 19:00:06 -0500

    test2

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -3,8 +3,7 @@ include 'include.php';
 
 // $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 $select = '*';
-$where = [];
 
-json_return( select_sql( $select, 'attendees', $where ) );
+echo select_sql( $select, 'attendees', null );
 
 ?>
