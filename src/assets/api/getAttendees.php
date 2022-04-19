4a737cb036a2379565416831e6d296923ec0994a
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 19:33:24 -0500

    now use the join

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -1,9 +1,7 @@
 <?php
 include 'include.php';
 
-// $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
-$select = '*';
+$sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 
-json_return( select_sql( $select, 'attendees', null ) );
-
+json_return( sql_submit( $sql ) );
 ?>
