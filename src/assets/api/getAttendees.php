d19f5b54a5432d0a5e5eeb6321a5546c01c99dc9
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 20:17:46 -0500

    super specificity

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -1,7 +1,7 @@
 <?php
 include 'include.php';
 
-$sql = "SELECT `attendees`.*, `reg_txn`.* FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
+$sql = "SELECT attendees.attendee_id, attendees.first_name, attendees.last_name, reg_txn.attendee_id AS con_id, reg_txn.badge_id, reg_txn.timestamp FROM attendees LEFT JOIN reg_txn ON attendees.attendee_id = reg_txn.attendee_id";
 
 json_return( sql_submit( $sql ) )
 
