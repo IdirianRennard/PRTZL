552bf934bae564601d016447e4a5d772e2ec529d
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 19:15:32 -0500

    scratch that reverse it

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -4,6 +4,6 @@ include 'include.php';
 // $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 $select = '*';
 
-return_json( select_sql( $select, 'attendees', null ) );
+json_return( select_sql( $select, 'attendees', null ) );
 
 ?>
