b2f3e4c060cccd31eabf08fcb30e5fa5560357da
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 20:08:15 -0500

    change how you see things

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -3,5 +3,7 @@ include 'include.php';
 
 $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 
-json_return( sql_submit( $sql ) );
+echo sql_submit( $sql );
+
+// $return = array_map( null, sql_submit( $sql ) );
 ?>
