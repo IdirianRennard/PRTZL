3ac251a6c79d4eef4cad50b593d8d2c66622a71c
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 19:12:51 -0500

    return_json

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -4,6 +4,6 @@ include 'include.php';
 // $sql = "SELECT * FROM `attendees` LEFT JOIN `reg_txn` ON attendees.attendee_id = reg_txn.attendee_id";
 $select = '*';
 
-echo_json( select_sql( $select, 'attendees', null ) );
+return_json( select_sql( $select, 'attendees', null ) );
 
 ?>
