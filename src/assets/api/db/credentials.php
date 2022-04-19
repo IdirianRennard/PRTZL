8440f35ad2d1841f92d9a014bcbffd40c6016d15
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 19:21:00 -0500

    ... mf

--- src/assets/api/db/credentials.php
+++ src/assets/api/db/credentials.php
@@ -1,8 +1,8 @@
 <?php
 
-$db = 'db683169781';
-$host = 'db683169781.db.1and1.com:3306';
-$user = 'dbo683169781';
-$pwd = 'Al105thR1s3nPF1))!';
+$db = 'dbo687177883';
+$host = 'db687177883.db.1and1.com:3306';
+$user = 'dbo687177883';
+$pwd = 'Ath3n4ch3w55t1ck5'
 
 ?>
