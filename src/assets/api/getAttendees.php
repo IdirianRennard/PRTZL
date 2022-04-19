c0e59abdbabe3b8533842215dd202f91e3a36bac
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-04-18 21:42:42 -0500

    try without glasses

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -1,8 +1,25 @@
 <?php
 include 'include.php';
 
-$sql = "SELECT attendees.attendee_id, attendees.first_name, attendees.last_name, reg_txn.attendee_id AS con_id, reg_txn.badge_id, reg_txn.timestamp FROM attendees LEFT JOIN reg_txn ON attendees.attendee_id = reg_txn.attendee_id";
+function mapAttendee ( $obj ) {
+    $attendee = new stdClass();
+    $attendee->attendee_id = $obj.attendee_id;
+    $attendee->first_name = $obj.first_name;
+    $attendee->last_name = $obj.last_name;
 
-json_return( sql_submit( $sql ) )
+    if ( !is_null( $obj.con_id ) ) {
+        $attendee->badge->id = $obj.badge_id;
+        $attendee->badge->timestamp = $obj.timestamp;
+    } else {
+        $attendee->badge = null;
+    }
+
+    return $attendee
+
+}
+
+$sql = "SELECT attendees.attendee_id, attendees.first_name, attendees.last_name, reg_txn.attendee_id AS con_id, reg_txn.badge_id, reg_txn.timestamp FROM attendees LEFT JOIN reg_txn ON attendees.attendee_id = reg_txn.attendee_id" ORDER BY attendees.attendee_id ASC”;
+
+json_return( array_map( ‘mapAttendee’, sql_submit( $sql ) );
 
 ?>
