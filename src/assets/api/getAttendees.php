a0e9d873d2f5c9838657d0c413d46a6006ceb471
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-05-01 11:15:02 -0500

    array_map

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -48,29 +48,6 @@ for( $i = 1; $i <= $pages->total_pages ; $i++ ){
   }
 }
 
-print_r( $tteBadges );
-// $attendee = [];
+json_return( array_map( 'mapAttendee', $tteBadges ));
 
-// for( $i = 0 ; $i < count( $tteBadges ) ; $i++ ){
-
-//   $scr = new Attendee (
-//     $tteBadges[$i]->badge_number,
-//     $tteBadges[$i]->firstname,
-//     $tteBadges[$i]->lastname,
-//     $$tteBadges[$i]->email,
-//     null
-//   );
-
-
-//   array_push( $attendee, $scr );
-// }
-
-// json_return( $attendee );
-
-// $sql = "SELECT attendees.attendee_id, attendees.first_name, attendees.last_name, reg_txn.attendee_id AS con_id, reg_txn.badge_id, reg_txn.timestamp
-//         FROM attendees
-//         LEFT JOIN reg_txn ON attendees.attendee_id = reg_txn.attendee_id
-//         ORDER BY attendees.attendee_id ASC";
-
-// json_return( array_map( 'mapAttendee', $tteBadges ));
 ?>
