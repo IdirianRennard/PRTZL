52149b0105ca4f42ec5f1b8a306d8a014041f432
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-05-01 18:58:53 -0500

    postReg is go

--- src/assets/api/postReg.php
+++ src/assets/api/postReg.php
@@ -2,8 +2,15 @@
 include 'include.php';
 
 $fp = fopen( 'php://input', 'r');
-$rawData = stream_get_contents($fp);
+$post = stream_get_contents($fp);
+$post = json_decode($post);
 
-print_r( json_decode( $rawData ) );
+$insert = [
+  'attendee_id' =>  $post[ 'id '],
+  'barcode'     =>  $post[ 'barcode' ],
+  'timestamp'   =>  $time
+];
+
+json_return( insert_sql( $insert, 'reg_txn' ));
 
 ?>
