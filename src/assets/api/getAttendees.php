cf8a201fcd5a95e224a695b8805773af0473aa2e
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-05-16 19:34:59 -0500

    rtfd Nate

--- src/assets/api/getAttendees.php
+++ src/assets/api/getAttendees.php
@@ -33,37 +33,38 @@ $badges = get_call( "$badgesUrl&page=0" );
 $badges = json_decode( $badges );
 $pages = $badges->result->paging;
 $totalPages = $pages->total_pages;
+$totalItems = $pages->total_items;
 
 $items = $badges->result->items;
 $tteBadges = [];
 
-// foreach( $items as $k => $v ) {
+foreach( $items as $k => $v ) {
 
-//   $where = [];
-//   $where["attendee_id"] = $v->badge_number;
+  $where = [];
+  $where["attendee_id"] = $v->badge_number;
 
-//   $select = [
-//     'attendee_id',
-//     'barcode',
-//     'timestamp',
-//   ];
+  $select = [
+    'attendee_id',
+    'barcode',
+    'timestamp',
+  ];
 
-//   $txn = array_map( 'mapTxn', select_sql( $select, "reg_txn",  $where ) );
-//   usort( $txn, fn( $a, $b) => $b->timestamp - $a->timestamp );
+  $txn = array_map( 'mapTxn', select_sql( $select, "reg_txn",  $where ) );
+  usort( $txn, fn( $a, $b) => $b->timestamp - $a->timestamp );
 
-//   $attendee = new Attendee();
+  $attendee = new Attendee();
 
-//   $attendee->id         = (int)$v->badge_number;
-//   $attendee->first_name = $v->firstname;
-//   $attendee->last_name  = $v->lastname;
-//   $attendee->barcode    = $txn;
+  $attendee->id         = (int)$v->badge_number;
+  $attendee->first_name = $v->firstname;
+  $attendee->last_name  = $v->lastname;
+  $attendee->barcode    = $txn;
 
-//   $tteBadges[ $attendee->id ] = $attendee;
-// }
+  $tteBadges[ $attendee->id ] = $attendee;
+}
 
-for( $i = 1; $i < $totalPages + 1 ; $i++ ){
-  json_echo( "Page $i of $totalPages" );
-  $pagedBadgesUrl = "$badgesUrl&page=$i";
+for( $i = 2; $i < $totalPages + 1 ; $i++ ){
+  json_echo( "Page $i of $totalPages\n\n" );
+  $pagedBadgesUrl = "$badgesUrl&_page_number=$i";
   $pagedBadges = get_call( $pagedBadgesUrl );
   $pagedBadges = json_decode( $pagedBadges );
 
