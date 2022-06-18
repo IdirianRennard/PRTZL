a3b9801f7920acce257e7951fbcedc3d4e953806
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-06-18 14:22:21 -0500

    um2 electric boogaloo

--- src/assets/api/include.php
+++ src/assets/api/include.php
@@ -5,6 +5,6 @@ include './db/db_scr.php';
 $time = gmdate( 'Y-M-d_H:i:s_T' );
 
 header( 'Content-Type: application/json'        );
-header( 'Access-Control-Allow-Methods: POST'    );
+header( 'Access-Control-Allow-Methods: GET, POST'    );
 header( 'Access-Control-Allow-Origin: *'        );
 ?>
