89a242edf34c3edff460c19a8679db1f9ebebbbc
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-06-18 19:36:27 -0500

    CORS-V

--- src/assets/api/include.php
+++ src/assets/api/include.php
@@ -5,6 +5,8 @@ include './db/db_scr.php';
 $time = gmdate( 'Y-M-d_H:i:s_T' );
 
 header( 'Content-Type: application/json'        );
-header( 'Access-Control-Allow-Methods: *'       );
+header( 'Access-Control-Allow-Methods: GET, HEAD, OPTIONS, POST, PUT'       );
+header( 'Access-Control-Allow-Headers: *'       );
 header( 'Access-Control-Allow-Origin: *'        );
 ?>
+
