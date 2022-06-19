123fc7cb4efe6be039c83024ac4cb25a8859794c
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-06-18 19:14:12 -0500

    try this for CORS

--- src/assets/api/include.php
+++ src/assets/api/include.php
@@ -5,6 +5,14 @@ include './db/db_scr.php';
 $time = gmdate( 'Y-M-d_H:i:s_T' );
 
 header( 'Content-Type: application/json'        );
-header( 'Access-Control-Allow-Methods: GET, POST'    );
+header( 'Access-Control-Allow-Methods: GET, HEAD, OPTIONS, POST, PUT'    );
 header( 'Access-Control-Allow-Origin: *'        );
+header( 'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Headers,
+          Origin,Accept,
+          X-Requested-With,
+          Content-Type,
+          Access-Control-Request-Method,
+          Access-Control-Request-Headers'
+      );
 ?>
