e9d2793ff631f992fd145d58fcdeaaf540f0ac9b
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-06-18 19:31:07 -0500

    CORS fix attempt 4

--- src/assets/api/include.php
+++ src/assets/api/include.php
@@ -4,8 +4,7 @@ include './db/db_scr.php';
 
 $time = gmdate( 'Y-M-d_H:i:s_T' );
 
-header( 'Access-Control-Allow-Origin: *' );
-header( 'Access-Control-Allow-Methods: GET, HEAD, OPTIONS, POST, PUT' );
-header( 'Content-Type: application/json' );
-
+header( 'Content-Type: application/json'        );
+header( 'Access-Control-Allow-Methods: *'       );
+header( 'Access-Control-Allow-Origin: *'        );
 ?>
