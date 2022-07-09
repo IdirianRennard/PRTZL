171f92096cc0a723396b98460f16e2e5f6729d45
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-07-09 12:13:09 -0500

    update data to put in form, object changed

--- src/assets/api/postReg.php
+++ src/assets/api/postReg.php
@@ -6,8 +6,10 @@ $post = json_decode(file_get_contents('php://input'));
 print_r($post);
 
 $insert = [
-  'attendee_id' =>  $post->id,
-  'barcode'     =>  $post->barcode,
+  'attendee_id' =>  $post->conID,
+  'barcode'     =>  $post->formBarcode,
+  'timestamp'   =>  $time,
+  'txn_key'     =>  null,
 ];
 
 json_return(insert_sql($insert, 'reg_txn'));
