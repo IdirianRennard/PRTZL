7057386496f4435847893283070a313b7acf8cc4
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-07-09 12:24:22 -0500

    lets null the timestamp and see if that fixes it

--- src/assets/api/postReg.php
+++ src/assets/api/postReg.php
@@ -7,9 +7,10 @@ $post = json_decode(file_get_contents('php://input'));
 $insert = [
   'attendee_id' =>  $post->conID,
   'barcode'     =>  $post->formBarcode,
-  'timestamp'   =>  $time,
+  'timestamp'   =>  null,
   'txn_key'     =>  null,
 ];
+
 print_r($insert);
 
 json_return(insert_sql($insert, 'reg_txn'));
