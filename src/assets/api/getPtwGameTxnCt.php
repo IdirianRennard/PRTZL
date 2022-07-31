f7d02e87a76745422319f51324881e5f20029586
IdirianRennard <idirian@houserennard.online>
2022-07-31 13:56:10 -0500

    get game ct 2

--- src/assets/api/getPtwGameTxnCt.php
+++ src/assets/api/getPtwGameTxnCt.php
@@ -4,5 +4,13 @@ include 'include.php';
 $sql = "SELECT DISTINCT game_name FROM ptw_txn";
 
 $gameList = sql_submit($sql);
+$gameList = array_column($gameList, 'game_name');
 
-print_r($gameList);
+$return = [];
+foreach ($gameList as $game) {
+  $sql = "SELECT COUNT( $game ) FROM ptw_txn";
+
+  $gameCt = sql_submit($sql);
+
+  print_r($gameCt);
+}
