0c0a6377ee795f6537aa69cd5db4f990195b46aa
IdirianRennard <idirian@houserennard.online>
2022-07-31 14:29:40 -0500

    can i sort it tho

--- src/assets/api/getPtwGameTxnCt.php
+++ src/assets/api/getPtwGameTxnCt.php
@@ -1,6 +1,11 @@
 <?php
 include 'include.php';
 
+function sortBtCt($a, $b)
+{
+  return ($a->count < $b->count) ? -1 : 1;
+}
+
 $sql = "SELECT DISTINCT game_name FROM ptw_txn";
 
 $gameList = sql_submit($sql);
@@ -10,9 +15,14 @@ $return = [];
 foreach ($gameList as $game) {
   $sql = "SELECT COUNT(*) as '$game'  FROM ptw_txn WHERE game_name = '$game'";
 
-  $gameCt = sql_submit($sql);
+  $gameCt = sql_submit($sql)[0];
 
-  $return[] = $gameCt;
+  $e = new StdClass();
+  $e->game_name = $game;
+  $e->count = $gameCt[$game];
+
+  $return[] = $e;
 }
 
+usort($return, 'sortBtCt');
 json_return($return);
