db501b9a2de7933001c2539638a732845581c795
IdirianRennard <55301781+IdirianRennard@users.noreply.github.com>
2022-07-31 02:40:19 -0500

    just get the damn list

--- src/assets/api/getPtwTxn.php
+++ src/assets/api/getPtwTxn.php
@@ -1,62 +1,18 @@
 <?php
 include 'include.php';
 
-// function destring($str)
-// {
-//   return (int)$str;
-// }
+if (count($_GET) === 0) {
+  $where = null;
+} else {
+  $where = $_GET;
+}
 
-$sql = "SELECT Ptw.game_name, Attendee.attendee_id, Attendee.first_name, Attendee.last_name, Ptw.timestampFROM ptw_txn INNER JOIN attendees ON CAST(Ptw.attendee_id AS INT) = Attendee.attendee_id";
+$select = [
+  'game_name',
+  'attendee_id',
+  'timestamp'
+];
 
-json_return(sql_submit($sql));
+$ptwTxnList = select_sql($select, 'ptw_txn', $where);
 
-// class PtwTxn
-// {
-//   public $attendee_id;
-//   public $first_name;
-//   public $last_name;
-//   public $game_name;
-// }
-
-// if (count($_GET) === 0) {
-//   $where = null;
-// } else {
-//   $where = $_GET;
-// }
-
-// $select = [
-//   'game_name',
-//   'attendee_id',
-//   'timestamp'
-// ];
-
-// $ptwTxnList = select_sql($select, 'ptw_txn', $where);
-
-// $attendee_select = [
-//   'attendee_id',
-//   'first_name',
-//   'last_name',
-// ];
-
-// $return_array = [];
-
-// foreach ($ptwTxnList as $entry) {
-
-//   print_r($entry);
-
-//   $where = [
-//     'attendee_id' => (int)$entry->attendee_id,
-//   ];
-
-//   $attendee = select_sql($attendee_select, 'attendees', $where)[0];
-
-//   $scrTxn = new PtwTxn();
-//   $scrTxn->attendee_id = $entry->attendee_id;
-//   $scrTxn->first_name = $attendee->first_name;
-//   $scrTxn->last_name = $attendee->last_name;
-//   $scrTxn->game_name = $entry->game_name;
-
-//   $return_array[] = $scrTxn;
-// }
-
-// // json_return($return_array);
+json_return($ptwTxnList);
