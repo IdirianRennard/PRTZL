35ca07470c99885f798f67683f61cdb420abacbb
IdirianRennard <idirian@houserennard.online>
2023-02-11 14:14:12 -0600

    remove old google sheet things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { PlayerRegComponent } from './main/player-reg/player-reg.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
